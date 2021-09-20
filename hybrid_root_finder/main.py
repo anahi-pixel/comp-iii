4aa88d29e47552762594e1326676e155cd180fa1
Isabella Basso <isabellabdoamaral@usp.br>
2021-09-20 15:39:09 -0300

    add example physics equation for testing
    Signed-off-by: Isabella Basso <isabellabdoamaral@usp.br>

--- hybrid_root_finder/main.py
+++ hybrid_root_finder/main.py
@@ -1,8 +1,16 @@
 from approximate import FunctionApproximation
+from functions import FUNCTIONS
 
 
 def main(args: list[str]) -> None:
-    pass
+    if len(args) == 0:
+        function = FUNCTIONS[0]
+        print(function.meta)
+        approx = FunctionApproximation.dekkerMethod(
+            function.function(1, 10),  # m = 1, g = 10
+            -2, 0  # search interval
+        )
+        print(approx)
 
 
 if __name__ == '__main__':
