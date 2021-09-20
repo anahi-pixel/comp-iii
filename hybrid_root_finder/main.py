863269a8a03f6240a699b0766f46dd7952f1dff6
Isabella Basso <isabellabdoamaral@usp.br>
2021-09-20 18:56:38 -0300

    rewrite main
    add functionality to the main "module" so that it prints output nicely
    and is also able to read from standard input easily
    
    Signed-off-by: Isabella Basso <isabellabdoamaral@usp.br>

--- hybrid_root_finder/main.py
+++ hybrid_root_finder/main.py
@@ -1,28 +1,67 @@
 from approximate import FunctionApproximation
 from functions import FUNCTIONS
+from dataclasses import dataclass
 
 
-def main(args: list[str]) -> None:
-    if len(args) == 0:
-        function = FUNCTIONS[0]
-        print(function.meta)
-        approx = FunctionApproximation.dekkerMethod(
-            function.function(1, 10),  # m = 1, g = 10
-            -2, 0  # search interval
-        )
-        print(approx)
-        return
+@dataclass
+class Params:
+    function: int
+    interval: list[float] = None
+    constants: list[float] = None
 
-    function = FUNCTIONS[int(args[0])]
-    print(function.meta)
+    def __post_init__(self):
+        f = FUNCTIONS[self.function]
+        if self.interval is None:
+            self.interval = f.interval
+        if self.function < 3 and self.constants is None:
+            self.constants = f.constants
+
+
+def get_args(args: list[str]):
+    if len(args) < 1:
+        function_index = 0
+    else:
+        function_index = int(args[0])
+
+    args = [
+            float(i)
+            for i in args[1:]
+        ]
+
+    consts = None
+    interval = None
+
+    if len(args) == 2:
+        interval = args
+    elif len(args) > 2:
+        a, b, consts = args
+        interval = [a, b]
+
+    return Params(function_index, interval, consts)
+
+
+def main(params: Params) -> None:
+    function = FUNCTIONS[params.function]
+
+    print(function.meta.name)
+    print()
+    print(function.meta.context)
+    print()
+    print('> Calculating...')
+
     approx = FunctionApproximation.dekkerMethod(
-        function.function(1, 10),  # m = 1, g = 10
-        -2, 2  # search interval
-    )
-    print(approx)
+        function.function(*params.constants),
+        *params.interval
+        )
+
+    print('> Done!')
+    print()
+
+    print('For the interval:', tuple(params.interval))
+    print('Dekker method found a zero at:', approx)
 
 
 if __name__ == '__main__':
     import sys
 
-    main(sys.argv[1:])
+    main(get_args(sys.argv[1:]))
